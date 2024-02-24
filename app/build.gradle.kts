fda963fee011f0cae92c2c847cfb11b4458b6cbd
Filipe Belatti <fibelatti@gmail.com>
2024-02-24 22:06:23 +0000

    Bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ object AppInfo {
     const val APPLICATION_ID = "com.fibelatti.photowidget"
 
     private const val VERSION_MAJOR = 1
-    private const val VERSION_MINOR = 3
+    private const val VERSION_MINOR = 4
     private const val VERSION_PATCH = 0
     private const val VERSION_BUILD = 0
 
