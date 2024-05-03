eaadcb58a38e83a2a653c6bf2c7016716d2b43e3
Filipe Belatti <fibelatti@gmail.com>
2024-05-03 23:58:07 +0100

    Add heart shape

--- app/src/main/java/com/fibelatti/photowidget/model/PhotoWidgetShape.kt
+++ app/src/main/java/com/fibelatti/photowidget/model/PhotoWidgetShape.kt
@@ -2,18 +2,79 @@ package com.fibelatti.photowidget.model
 
 import androidx.annotation.IntRange
 
-data class PhotoWidgetShape(
-    val id: String,
-    val type: Type,
-    @IntRange(from = 3)
-    val numVertices: Int,
-    val roundness: Float,
-    val innerRadius: Float = .5f,
-    val rotation: Float = 0f,
-) {
+sealed class PhotoWidgetShape {
 
-    enum class Type {
-        POLYGON,
-        STAR,
+    abstract val id: String
+    abstract val rotation: Float
+    abstract val scaleX: Float
+    abstract val scaleY: Float
+
+    data class Polygon(
+        override val id: String,
+        @IntRange(from = 3)
+        val numVertices: Int,
+        val roundness: Float,
+        override val rotation: Float = 0f,
+        override val scaleX: Float = 1f,
+        override val scaleY: Float = 1f,
+    ) : PhotoWidgetShape()
+
+    data class Star(
+        override val id: String,
+        @IntRange(from = 3)
+        val numVertices: Int,
+        val roundness: Float,
+        val innerRadius: Float,
+        override val rotation: Float = 0f,
+        override val scaleX: Float = 1f,
+        override val scaleY: Float = 1f,
+    ) : PhotoWidgetShape()
+
+    data class Custom(
+        override val id: String,
+        val vertices: FloatArray,
+        val roundness: Float = 0f,
+        val perVertexRoundness: List<Float>? = null,
+        val perVertexSmoothing: List<Float>? = null,
+        override val rotation: Float = 0f,
+        override val scaleX: Float = 1f,
+        override val scaleY: Float = 1f,
+    ) : PhotoWidgetShape() {
+
+        init {
+            require(perVertexRoundness == null || perVertexRoundness.size == vertices.size / 2)
+            require(perVertexSmoothing == null || perVertexSmoothing.size == perVertexRoundness?.size)
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Custom
+
+            if (id != other.id) return false
+            if (!vertices.contentEquals(other.vertices)) return false
+            if (roundness != other.roundness) return false
+            if (perVertexRoundness != other.perVertexRoundness) return false
+            if (perVertexSmoothing != other.perVertexSmoothing) return false
+            if (rotation != other.rotation) return false
+            if (scaleX != other.scaleX) return false
+            if (scaleY != other.scaleY) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = id.hashCode()
+            result = 31 * result + vertices.contentHashCode()
+            result = 31 * result + roundness.hashCode()
+            result = 31 * result + (perVertexRoundness?.hashCode() ?: 0)
+            result = 31 * result + (perVertexSmoothing?.hashCode() ?: 0)
+            result = 31 * result + rotation.hashCode()
+            result = 31 * result + scaleX.hashCode()
+            result = 31 * result + scaleY.hashCode()
+            return result
+        }
+
     }
 }
--- app/src/main/java/com/fibelatti/photowidget/model/PhotoWidgetShapeBuilder.kt
+++ app/src/main/java/com/fibelatti/photowidget/model/PhotoWidgetShapeBuilder.kt
@@ -1,76 +1,90 @@
 package com.fibelatti.photowidget.model
 
 import android.graphics.Matrix
+import android.graphics.PointF
 import android.graphics.RectF
+import androidx.core.graphics.plus
+import androidx.core.graphics.times
 import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.RoundedPolygon
 import androidx.graphics.shapes.star
 import androidx.graphics.shapes.transformed
+import kotlin.math.PI
+import kotlin.math.cos
 import kotlin.math.min
+import kotlin.math.sin
 
 object PhotoWidgetShapeBuilder {
 
     private val shapes: List<PhotoWidgetShape> = listOf(
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "square",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 4,
             roundness = 0.05f,
             rotation = 45f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "rounded-square",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 4,
             roundness = 0.2f,
             rotation = 45f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "squircle",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 4,
             roundness = 0.6f,
             rotation = 45f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "circle",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 8,
             roundness = 1f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Star(
             id = "scallop",
-            type = PhotoWidgetShape.Type.STAR,
             numVertices = 12,
             roundness = .1f,
             innerRadius = .928f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Star(
             id = "medal",
-            type = PhotoWidgetShape.Type.STAR,
             numVertices = 8,
             roundness = .16f,
             innerRadius = .784f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Star(
             id = "clover",
-            type = PhotoWidgetShape.Type.STAR,
             numVertices = 4,
             roundness = .32f,
             innerRadius = .352f,
             rotation = 45f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "octagon",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 8,
             roundness = .16f,
         ),
-        PhotoWidgetShape(
+        PhotoWidgetShape.Polygon(
             id = "hexagon",
-            type = PhotoWidgetShape.Type.POLYGON,
             numVertices = 6,
             roundness = .16f,
+        ),
+        PhotoWidgetShape.Custom(
+            id = "heart",
+            vertices = floatArrayOf(
+                radialToCartesian(radius = 0.8f, angleRadians = 270f.toRadians()).x,
+                radialToCartesian(radius = 0.8f, angleRadians = 270f.toRadians()).y,
+                radialToCartesian(radius = 0.8f, angleRadians = 30f.toRadians()).x,
+                radialToCartesian(radius = 0.8f, angleRadians = 30f.toRadians()).y,
+                radialToCartesian(radius = 0f, angleRadians = 90f.toRadians()).x,
+                radialToCartesian(radius = 0f, angleRadians = 90f.toRadians()).y,
+                radialToCartesian(radius = 0.8f, angleRadians = 150f.toRadians()).x,
+                radialToCartesian(radius = 0.8f, angleRadians = 150f.toRadians()).y,
+            ),
+            perVertexRoundness = listOf(0f, .2f, 0f, .2f),
+            perVertexSmoothing = listOf(0f, 0.65f, 0f, 0.65f),
+            rotation = 180f,
+            scaleY = 0.8f,
         ),
     )
 
@@ -104,25 +118,49 @@ object PhotoWidgetShapeBuilder {
     )
 
     private fun buildShape(photoWidgetShape: PhotoWidgetShape): RoundedPolygon {
-        val polygon = when (photoWidgetShape.type) {
-            PhotoWidgetShape.Type.POLYGON -> RoundedPolygon(
+        val polygon = when (photoWidgetShape) {
+            is PhotoWidgetShape.Polygon -> RoundedPolygon(
                 numVertices = photoWidgetShape.numVertices,
                 rounding = CornerRounding(radius = photoWidgetShape.roundness),
             )
 
-            PhotoWidgetShape.Type.STAR -> RoundedPolygon.star(
+            is PhotoWidgetShape.Star -> RoundedPolygon.star(
                 numVerticesPerRadius = photoWidgetShape.numVertices,
                 innerRadius = photoWidgetShape.innerRadius,
                 rounding = CornerRounding(radius = photoWidgetShape.roundness),
+            )
+
+            is PhotoWidgetShape.Custom -> RoundedPolygon(
+                vertices = photoWidgetShape.vertices,
+                rounding = CornerRounding(radius = photoWidgetShape.roundness),
+                perVertexRounding = photoWidgetShape.perVertexRoundness?.mapIndexed { index, value ->
+                    CornerRounding(
+                        radius = value,
+                        smoothing = photoWidgetShape.perVertexSmoothing?.get(index) ?: 0f,
+                    )
+                },
             )
         }
 
         return polygon.transformed(
             matrix = Matrix().apply {
                 postRotate(photoWidgetShape.rotation)
+                postScale(photoWidgetShape.scaleX, photoWidgetShape.scaleY)
             },
         )
     }
+
+    private fun radialToCartesian(
+        radius: Float,
+        angleRadians: Float,
+        center: PointF = PointF(0f, 0f),
+    ): PointF = directionVectorPointF(angleRadians) * radius + center
+
+    private fun directionVectorPointF(
+        angleRadians: Float,
+    ): PointF = PointF(cos(angleRadians), sin(angleRadians))
+
+    private fun Float.toRadians() = this * PI.toFloat() / 180f
 }
 
 fun RoundedPolygon.transformed(
