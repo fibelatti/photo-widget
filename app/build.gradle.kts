4e36a692fd343a7b6885179b57731d05e6eede6e
Filipe Belatti <fibelatti@gmail.com>
2024-10-09 17:12:06 +0100

    Bump version to 1.15.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ object AppInfo {
 
     private const val VERSION_MAJOR = 1
     private const val VERSION_MINOR = 15
-    private const val VERSION_PATCH = 1
-    private const val VERSION_BUILD = 1
+    private const val VERSION_PATCH = 2
+    private const val VERSION_BUILD = 0
 
     val versionCode: Int =
         (VERSION_MAJOR * 1_000_000)
