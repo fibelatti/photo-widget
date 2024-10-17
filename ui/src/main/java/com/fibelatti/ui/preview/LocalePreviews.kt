fce0e24f532d31f89c745488be59a8c99dc3d79a
Filipe Belatti <fibelatti@gmail.com>
2024-10-17 23:08:28 +0100

    Add russian translation

--- ui/src/main/java/com/fibelatti/ui/preview/LocalePreviews.kt
+++ ui/src/main/java/com/fibelatti/ui/preview/LocalePreviews.kt
@@ -17,6 +17,13 @@ import androidx.compose.ui.tooling.preview.Preview
     locale = "es",
 )
 @Preview(
+    name = "Locale Preview - RU",
+    group = "Localization",
+    showSystemUi = true,
+    showBackground = true,
+    locale = "ru",
+)
+@Preview(
     name = "Locale Preview - TR",
     group = "Localization",
     showSystemUi = true,
