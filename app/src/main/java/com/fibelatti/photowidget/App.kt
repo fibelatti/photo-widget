d124bf9f813b34cc1da352bbb7e9db20277a0ed4
Filipe Belatti <fibelatti@gmail.com>
2024-10-26 17:51:06 +0100

    Add thread policy for `StrictMode`

--- app/src/main/java/com/fibelatti/photowidget/App.kt
+++ app/src/main/java/com/fibelatti/photowidget/App.kt
@@ -32,6 +32,13 @@ class App : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
 
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build(),
+            )
+
             StrictMode.setVmPolicy(
                 StrictMode.VmPolicy.Builder()
                     .detectAll()
