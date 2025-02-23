83fb02a8a3cd88f12a407a9aab03895256460d0b
Filipe Belatti <fibelatti@gmail.com>
2025-02-23 19:42:32 +0000

    Bump version to 1.25.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [v1.25.0] - 2025-02-23
+[v1.25.0]: https://github.com/fibelatti/photo-widget/releases/tag/v1.25.0
+
 ### Added
 
 * Add support for HEIC/HEIF file format when syncing folders
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ object AppInfo {
     const val APPLICATION_ID = "com.fibelatti.photowidget"
 
     private const val VERSION_MAJOR = 1
-    private const val VERSION_MINOR = 24
-    private const val VERSION_PATCH = 2
+    private const val VERSION_MINOR = 25
+    private const val VERSION_PATCH = 0
     private const val VERSION_BUILD = 0
 
     val versionCode: Int = (VERSION_MAJOR * 1_000_000)
