4961372da7074aba10c14546feaa918a290aef84
Filipe Belatti <fibelatti@gmail.com>
2025-06-15 16:45:56 +0100

    Bump version to 1.31.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [v1.31.0] - 2025-06-15
+[v1.31.0]: https://github.com/fibelatti/photo-widget/releases/tag/v1.31.0
+
 ### Added
 
 * Many new shapes
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,9 @@ object AppInfo {
     const val APPLICATION_ID = "com.fibelatti.photowidget"
 
     private const val VERSION_MAJOR = 1
-    private const val VERSION_MINOR = 30
+    private const val VERSION_MINOR = 31
     private const val VERSION_PATCH = 0
-    private const val VERSION_BUILD = 1
+    private const val VERSION_BUILD = 0
 
     val versionCode: Int = (VERSION_MAJOR * 1_000_000)
         .plus(VERSION_MINOR * 10_000)
