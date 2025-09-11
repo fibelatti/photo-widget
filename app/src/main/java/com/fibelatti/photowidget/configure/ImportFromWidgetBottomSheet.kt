6db6b22e2f8e3f722644ce93849c3a91aeb386fc
Filipe Belatti <fibelatti@gmail.com>
2025-09-11 11:57:48 +0100

    Update artifacts

--- app/src/main/java/com/fibelatti/photowidget/configure/ImportFromWidgetBottomSheet.kt
+++ app/src/main/java/com/fibelatti/photowidget/configure/ImportFromWidgetBottomSheet.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fibelatti.photowidget.R
 import com.fibelatti.photowidget.home.HomeViewModel
--- app/src/main/java/com/fibelatti/photowidget/platform/AppTheme.kt
+++ app/src/main/java/com/fibelatti/photowidget/platform/AppTheme.kt
@@ -2,7 +2,7 @@ package com.fibelatti.photowidget.platform
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fibelatti.photowidget.preferences.UserPreferences
