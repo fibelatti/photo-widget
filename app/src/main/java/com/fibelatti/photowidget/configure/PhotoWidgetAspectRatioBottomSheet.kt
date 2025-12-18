b02ff6e5feed13b71a9af104bd00a7bf0f241e2d
Filipe Belatti <fibelatti@gmail.com>
2025-12-18 15:48:05 +0000

    Add scroll hint to the format picker

--- app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetAspectRatioBottomSheet.kt
+++ app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetAspectRatioBottomSheet.kt
@@ -2,12 +2,16 @@ package com.fibelatti.photowidget.configure
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -17,6 +21,7 @@ import com.fibelatti.photowidget.model.PhotoWidgetAspectRatio
 import com.fibelatti.ui.foundation.AppBottomSheet
 import com.fibelatti.ui.foundation.AppSheetState
 import com.fibelatti.ui.foundation.hideBottomSheet
+import com.fibelatti.ui.text.AutoSizeText
 
 @Composable
 fun PhotoWidgetAspectRatioBottomSheet(
@@ -57,5 +62,26 @@ private fun AspectRatioPickerContent(
             onAspectRatioSelected = onAspectRatioSelected,
             modifier = Modifier.fillMaxWidth(),
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(space = 4.dp, alignment = Alignment.End),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AutoSizeText(
+                text = stringResource(R.string.hint_scroll_to_view_more),
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 1,
+            )
+
+            Icon(
+                painter = painterResource(R.drawable.ic_chevron_right),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface,
+            )
+        }
     }
 }
