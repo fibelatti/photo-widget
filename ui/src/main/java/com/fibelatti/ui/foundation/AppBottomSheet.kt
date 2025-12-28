31a75393ab975a46386660ba34e19f545cdafe43
Filipe Belatti <fibelatti@gmail.com>
2025-12-28 13:33:27 +0000

    Disable overscroll on `AppBottomSheet`
    There's a weird lag happening which looks bad. Needs further investigation to clarify whether it's a platform or implementation issue.

--- ui/src/main/java/com/fibelatti/ui/foundation/AppBottomSheet.kt
+++ ui/src/main/java/com/fibelatti/ui/foundation/AppBottomSheet.kt
@@ -2,12 +2,14 @@
 
 package com.fibelatti.ui.foundation
 
+import androidx.compose.foundation.LocalOverscrollFactory
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -67,16 +69,21 @@ fun AppBottomSheet(
         return
     }
 
-    ModalBottomSheet(
-        onDismissRequest = {
-            // The sheet was dismissed with a gesture or click outside: clean up the custom state and notify the caller
-            sheetState.isVisible = false
-            onDismissRequest()
-        },
-        modifier = modifier,
-        sheetState = sheetState.state,
-        content = content,
-    )
+    CompositionLocalProvider(
+        LocalOverscrollFactory provides null,
+    ) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                // The sheet was dismissed with a gesture or click outside;
+                // Clean up the custom state and notify the caller
+                sheetState.isVisible = false
+                onDismissRequest()
+            },
+            modifier = modifier,
+            sheetState = sheetState.state,
+            content = content,
+        )
+    }
 }
 
 /**
