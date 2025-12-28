dcf8ccce77a439213049cf694033e9568a24bb69
Filipe Belatti <fibelatti@gmail.com>
2025-12-28 13:29:04 +0000

    Update the appearance of the tap action area

--- app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetTapActionPicker.kt
+++ app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetTapActionPicker.kt
@@ -14,7 +14,6 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -54,9 +53,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -398,16 +401,25 @@ private fun TapAreaIndicator(
                 .alpha(.6F),
         )
 
+        val areaRadius: Float = 28.dp.dpToPx()
+        val areaColor: Color = MaterialTheme.colorScheme.onSurface
+
         Row(
             modifier = Modifier
                 .width(280.dp)
                 .fillMaxHeight()
-                .clip(MaterialTheme.shapes.large)
-                .border(
-                    width = 1.dp,
-                    color = MaterialTheme.colorScheme.primary,
-                    shape = MaterialTheme.shapes.large,
-                ),
+                .drawWithContent {
+                    drawRoundRect(
+                        color = areaColor,
+                        cornerRadius = CornerRadius(areaRadius),
+                        style = Stroke(
+                            width = 1.5.dp.toPx(),
+                            pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f),
+                        ),
+                    )
+                    drawContent()
+                }
+                .clip(shape = RoundedCornerShape(size = areaRadius)),
         ) {
             Box(
                 modifier = Modifier
