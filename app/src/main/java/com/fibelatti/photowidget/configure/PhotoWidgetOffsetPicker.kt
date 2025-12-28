c294e89ee27ddb173d8efbd62cede990a0b2c116
Filipe Belatti <fibelatti@gmail.com>
2025-12-28 00:00:37 +0000

    Update the offset picker appearance

--- app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetOffsetPicker.kt
+++ app/src/main/java/com/fibelatti/photowidget/configure/PhotoWidgetOffsetPicker.kt
@@ -2,27 +2,16 @@
 
 package com.fibelatti.photowidget.configure
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.FilledTonalIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -33,17 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.fibelatti.photowidget.R
 import com.fibelatti.photowidget.model.PhotoWidget
 import com.fibelatti.photowidget.preferences.DefaultPicker
+import com.fibelatti.photowidget.ui.NumberSpinner
 import com.fibelatti.photowidget.ui.WidgetPositionViewer
 import com.fibelatti.ui.preview.AllPreviews
 import com.fibelatti.ui.theme.ExtendedTheme
@@ -62,41 +46,50 @@ fun PhotoWidgetOffsetPicker(
         var horizontalValue by remember(horizontalOffset) { mutableIntStateOf(horizontalOffset) }
         var verticalValue by remember(verticalOffset) { mutableIntStateOf(verticalOffset) }
 
-        BoxWithConstraints {
-            if (maxWidth < 600.dp) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    PhotoWidgetOffsetViewer(
-                        horizontalValue = horizontalValue,
-                        verticalValue = verticalValue,
-                    )
+        PhotoWidgetOffsetViewer(
+            horizontalValue = horizontalValue,
+            verticalValue = verticalValue,
+        )
 
-                    PhotoWidgetOffsetControls(
-                        horizontalValue = horizontalValue,
-                        verticalValue = verticalValue,
-                        onHorizontalValueChange = { horizontalValue = it },
-                        onVerticalValueChange = { verticalValue = it },
-                    )
-                }
-            } else {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    PhotoWidgetOffsetViewer(
-                        horizontalValue = horizontalValue,
-                        verticalValue = verticalValue,
-                        modifier = Modifier.weight(0.4f),
-                    )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.photo_widget_configure_offset_current_horizontal),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                )
 
-                    PhotoWidgetOffsetControls(
-                        horizontalValue = horizontalValue,
-                        verticalValue = verticalValue,
-                        onHorizontalValueChange = { horizontalValue = it },
-                        onVerticalValueChange = { verticalValue = it },
-                        modifier = Modifier.weight(0.6f),
-                    )
-                }
+                NumberSpinner(
+                    value = horizontalValue,
+                    onIncreaseClick = { horizontalValue++ },
+                    onDecreaseClick = { horizontalValue-- },
+                )
+            }
+
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.photo_widget_configure_offset_current_vertical),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+
+                NumberSpinner(
+                    value = verticalValue,
+                    onIncreaseClick = { verticalValue++ },
+                    onDecreaseClick = { verticalValue-- },
+                )
             }
         }
 
@@ -145,151 +138,6 @@ private fun PhotoWidgetOffsetViewer(
             .width(200.dp)
             .aspectRatio(.75f),
     )
-}
-
-@Composable
-private fun PhotoWidgetOffsetControls(
-    horizontalValue: Int,
-    verticalValue: Int,
-    onHorizontalValueChange: (Int) -> Unit,
-    onVerticalValueChange: (Int) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val localHaptics = LocalHapticFeedback.current
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(all = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(32.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Box(
-            contentAlignment = Alignment.Center,
-        ) {
-            Column(
-                modifier = Modifier
-                    .height(144.dp)
-                    .width(48.dp),
-            ) {
-                FilledTonalIconButton(
-                    onClick = {
-                        onVerticalValueChange(verticalValue - 1)
-                        localHaptics.performHapticFeedback(HapticFeedbackType.VirtualKey)
-                    },
-                    shapes = IconButtonDefaults.shapes(),
-                    modifier = Modifier.size(48.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_down),
-                        contentDescription = null,
-                        modifier = Modifier.rotate(180f),
-                    )
-                }
-
-                Spacer(modifier = Modifier.size(48.dp))
-
-                FilledTonalIconButton(
-                    onClick = {
-                        onVerticalValueChange(verticalValue + 1)
-                        localHaptics.performHapticFeedback(HapticFeedbackType.VirtualKey)
-                    },
-                    shapes = IconButtonDefaults.shapes(),
-                    modifier = Modifier.size(48.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_down),
-                        contentDescription = null,
-                    )
-                }
-            }
-
-            Row(
-                modifier = Modifier
-                    .width(144.dp)
-                    .height(48.dp),
-            ) {
-                FilledTonalIconButton(
-                    onClick = {
-                        onHorizontalValueChange(horizontalValue - 1)
-                        localHaptics.performHapticFeedback(HapticFeedbackType.VirtualKey)
-                    },
-                    shapes = IconButtonDefaults.shapes(),
-                    modifier = Modifier.size(48.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_left),
-                        contentDescription = null,
-                    )
-                }
-
-                Spacer(modifier = Modifier.size(48.dp))
-
-                FilledTonalIconButton(
-                    onClick = {
-                        onHorizontalValueChange(horizontalValue + 1)
-                        localHaptics.performHapticFeedback(HapticFeedbackType.VirtualKey)
-                    },
-                    shapes = IconButtonDefaults.shapes(),
-                    modifier = Modifier.size(48.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_chevron_right),
-                        contentDescription = null,
-                    )
-                }
-            }
-        }
-
-        Column(
-            modifier = Modifier.weight(1f),
-        ) {
-            Text(
-                text = stringResource(
-                    id = R.string.photo_widget_configure_offset_current_horizontal,
-                    horizontalValue,
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceContainerHigh,
-                        shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.primaryContainer,
-                        shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
-                    )
-                    .padding(horizontal = 8.dp, vertical = 4.dp),
-                color = MaterialTheme.colorScheme.onSurface,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyLarge,
-            )
-
-            Text(
-                text = stringResource(
-                    id = R.string.photo_widget_configure_offset_current_vertical,
-                    verticalValue,
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceContainerHigh,
-                        shape = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp),
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.primaryContainer,
-                        shape = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp),
-                    )
-                    .padding(horizontal = 8.dp, vertical = 4.dp),
-                color = MaterialTheme.colorScheme.onSurface,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyLarge,
-            )
-        }
-    }
 }
 
 @Composable
--- app/src/main/java/com/fibelatti/photowidget/ui/NumberSpinner.kt
+++ app/src/main/java/com/fibelatti/photowidget/ui/NumberSpinner.kt
@@ -1,18 +1,16 @@
 package com.fibelatti.photowidget.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,6 +20,7 @@ import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.fibelatti.photowidget.R
+import com.fibelatti.ui.text.AutoSizeText
 
 @Composable
 fun NumberSpinner(
@@ -37,16 +36,13 @@ fun NumberSpinner(
 
     Row(
         modifier = modifier.height(IntrinsicSize.Max),
-        horizontalArrangement = Arrangement.spacedBy(1.dp),
     ) {
         FilledTonalIconButton(
             onClick = {
                 onDecreaseClick()
                 localHapticFeedback.performHapticFeedback(HapticFeedbackType.SegmentTick)
             },
-            modifier = Modifier
-                .fillMaxHeight()
-                .widthIn(min = 60.dp),
+            modifier = Modifier.fillMaxHeight(),
             enabled = lowerBound == null || value > lowerBound,
             shape = MaterialTheme.shapes.medium.copy(topEnd = zeroCorner, bottomEnd = zeroCorner),
         ) {
@@ -56,14 +52,15 @@ fun NumberSpinner(
         Box(
             modifier = Modifier
                 .fillMaxHeight()
-                .widthIn(min = 60.dp)
+                .width(48.dp)
                 .background(color = MaterialTheme.colorScheme.surfaceContainerHigh),
             contentAlignment = Alignment.Center,
         ) {
-            Text(
+            AutoSizeText(
                 text = value.toString(),
                 color = MaterialTheme.colorScheme.onSurface,
                 style = MaterialTheme.typography.titleLarge,
+                maxLines = 1,
             )
         }
 
@@ -72,9 +69,7 @@ fun NumberSpinner(
                 onIncreaseClick()
                 localHapticFeedback.performHapticFeedback(HapticFeedbackType.SegmentTick)
             },
-            modifier = Modifier
-                .fillMaxHeight()
-                .widthIn(min = 60.dp),
+            modifier = Modifier.fillMaxHeight(),
             enabled = upperBound == null || value < upperBound,
             shape = MaterialTheme.shapes.medium.copy(topStart = zeroCorner, bottomStart = zeroCorner),
         ) {
