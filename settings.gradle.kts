a601cacfeb95d37773483963ca61f7a31c89e415
Filipe Belatti <fibelatti@gmail.com>
2026-04-01 20:09:40 +0100

    Migrate to Gradle Daemon toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,6 +32,10 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":app")
