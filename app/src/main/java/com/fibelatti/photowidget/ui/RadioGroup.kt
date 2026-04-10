8ae1db2cbd076538ce874b8c5e862713dd561272
Filipe Belatti <fibelatti@gmail.com>
2026-04-10 19:47:49 +0100

    Update `RadioGroup` appearance

--- app/src/main/java/com/fibelatti/photowidget/ui/RadioGroup.kt
+++ app/src/main/java/com/fibelatti/photowidget/ui/RadioGroup.kt
@@ -1,7 +1,7 @@
 package com.fibelatti.photowidget.ui
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,12 +15,15 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
+import com.fibelatti.ui.foundation.Shapes
 
 @Composable
 fun <T : Any> RadioGroup(
@@ -30,23 +33,22 @@ fun <T : Any> RadioGroup(
     itemTitle: (T) -> String,
     modifier: Modifier = Modifier,
     itemDescription: (T) -> String? = { null },
-    backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainerHigh,
-    outlineColor: Color = MaterialTheme.colorScheme.outlineVariant,
-    shape: Shape = MaterialTheme.shapes.small,
 ) {
     Column(
         modifier = modifier.selectableGroup(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
     ) {
-        for (item in items) {
+        for ((index: Int, item: T) in items.withIndex()) {
             RadioGroupItem(
                 selected = itemSelected(item),
                 onClick = { onItemClick(item) },
                 title = itemTitle(item),
                 description = itemDescription(item),
-                backgroundColor = backgroundColor,
-                outlineColor = outlineColor,
-                shape = shape,
+                shape = when (index) {
+                    0 -> Shapes.TopShape
+                    items.lastIndex -> Shapes.BottomShape
+                    else -> Shapes.MiddleShape
+                },
             )
         }
     }
@@ -59,28 +61,27 @@ private fun RadioGroupItem(
     title: String,
     description: String?,
     modifier: Modifier = Modifier,
-    backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainerHigh,
-    outlineColor: Color = MaterialTheme.colorScheme.outlineVariant,
     shape: Shape = MaterialTheme.shapes.small,
 ) {
+    val backgroundColor: Color by animateColorAsState(
+        targetValue = if (selected) {
+            MaterialTheme.colorScheme.primaryContainer
+        } else {
+            MaterialTheme.colorScheme.surfaceContainerHigh
+        }
+    )
+
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .heightIn(min = 48.dp)
-            .background(
-                color = backgroundColor,
-                shape = shape,
-            )
-            .border(
-                width = 1.dp,
-                color = outlineColor,
-                shape = shape,
-            )
+            .heightIn(min = 60.dp)
+            .background(color = backgroundColor, shape = shape)
             .selectable(
                 selected = selected,
                 role = Role.RadioButton,
                 onClick = onClick,
             )
+            .clip(shape)
             .padding(horizontal = 16.dp, vertical = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalAlignment = Alignment.CenterVertically,
