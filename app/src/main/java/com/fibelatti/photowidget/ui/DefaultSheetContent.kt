5ecac516acf07a81a268458f95108127f7f150e2
Filipe Belatti <fibelatti@gmail.com>
2026-04-10 19:47:32 +0100

    Fix `DefaultSheetContent` max height logic

--- app/src/main/java/com/fibelatti/photowidget/ui/DefaultSheetContent.kt
+++ app/src/main/java/com/fibelatti/photowidget/ui/DefaultSheetContent.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -37,7 +38,7 @@ fun DefaultSheetContent(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .fillMaxHeight(fraction = .8f)
+            .heightIn(max = LocalWindowInfo.current.containerDpSize.height * .8f)
             .nestedScroll(rememberNestedScrollInteropConnection())
             .verticalScroll(scrollState)
             .fadingEdges(scrollState)
