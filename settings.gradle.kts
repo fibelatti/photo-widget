6831bf0d5c13af81e6301936d3dc711741b1dd0e
Filipe Belatti <fibelatti@gmail.com>
2026-05-08 23:22:43 +0100

    Revert "Migrate to Gradle Daemon toolchain"
    This reverts commit a601cacfeb95d37773483963ca61f7a31c89e415.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,10 +32,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":app")
