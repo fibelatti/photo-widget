7efa36fc0a226277a16a2568cc4a0decb3fb6a11
Filipe Belatti <fibelatti@gmail.com>
2026-06-20 10:52:55 +0100

    Bump version to 1.43.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [v1.43.0] - 2026-06-20
+[v1.43.0]: https://github.com/fibelatti/photo-widget/releases/tag/v1.43.0
+
 ### Added
 
 * Ability to select app shortcuts when using the "Open an app" tap action
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ object AppInfo {
     const val APPLICATION_ID = "com.fibelatti.photowidget"
 
     private const val VERSION_MAJOR = 1
-    private const val VERSION_MINOR = 42
-    private const val VERSION_PATCH = 4
+    private const val VERSION_MINOR = 43
+    private const val VERSION_PATCH = 0
     private const val VERSION_BUILD = 0
 
     val versionCode: Int = (VERSION_MAJOR * 1_000_000)
